96a08b52e35c9786065a56f0f61693810152a342
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-03-22 13:58:46 +0000

    Add comment form post method

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'blog',
 ]
@@ -57,6 +58,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRIPSY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
