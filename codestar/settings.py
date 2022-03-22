50e33715b2d7727bb3bdf48d6c6c2e9bc6cf9fef
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-03-22 13:05:51 +0000

    Add allauth login, logout, and signup pages

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     # cloudinary_storage must go above django.contribe.staticfiles
     'cloudinary_storage',
     'django.contrib.staticfiles',
@@ -48,6 +52,11 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
